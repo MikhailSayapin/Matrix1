30ffab67c3f42267c7f9d75a1a881971cc1cbd38
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-12-14 20:23:05 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -6,51 +6,53 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+
     if (argc == 1)
     {
-        cout << " Матрица пустая " << endl;
+        cout << " Матрица пустая. " << endl;
         return 0;
     }
-    
+
     string str1 = "\0", str2 = "\0";
-    int i, j;
-    int k;
-    for(k=0; argv[1][k]!='*'; k++)
+    int i, j, k; // k используется на всём протяжении программы для разных целей.
+    for (k = 0; argv[1][k] != '*'; k++)
     {
-         str1 += argv[1][k];
+        str1 += argv[1][k];
     }
 
     k++;
 
-    while(k!=strlen(argv[1]))
+    while (k != strlen(argv[1]))
     {
         str2 += argv[1][k];
         k++;
     }
 
-    k=2;
+    k = 2;
 
-    int x = atoi(str1.c_str());
-    int y = atoi(str2.c_str());
+    int* x = new int;
+    (*x) = atoi(str1.c_str());
+    int* y = new int;
+    (*y) = atoi(str2.c_str());
 
-    int arr[x][y];
-    
-    if ((argc-2) > (x*y))
+    int arr[(*x)][(*y)];
+
+    if ((argc - 2) > ((*x) * (*y)))
     {
-        cout << " Переполнение элементами матрицы размера " << x << "*" << y << endl;
+        cout << " Переполнение элементами матрицы размера " << (*x) << "*" << (*y) << endl;
         return 0;
     }
-    
-    if ((argc-2) < (x*y))
+
+    if ((argc - 2) < ((*x) * (*y)))
     {
-        cout << " Недостаточно элементов для матрицы размера " << x << "*" << y << endl;
+        cout << " Недостаточно элементов для матрицы размера " << (*x) << "*" << (*y) << endl;
         return 0;
     }
-        
 
-    for(i=0; i<x; i++)
+
+    for (i = 0; i < (*x); i++)
     {
-        for(j=0; j<y; j++)
+        for (j = 0; j < (*y); j++)
         {
             arr[i][j] = atoi(argv[k]);
             k++;
@@ -58,14 +60,35 @@ int main(int argc, char* argv[])
     }
 
 
-    for(i=0; i<x; i++)
+    cout << "Выберите одну из операций:" << endl;
+    cout << "1. Вывести матрицу" << endl;
+    cout << "2. Сложить матрицу" << endl;
+    cout << "3. Умножить матрицу" << endl;
+    cout << "4. Транспонировать матрицу" << endl;
+    cout << "5. Сохранить в файл" << endl;
+    cout << "6. Загрузить из файла" << endl;
+    cout << "7. Сортировать матрицу" << endl;
+    cin >> k;
+    switch (k)
     {
-        for(j=0; j<y; j++)
+        case 1:
         {
-            cout << arr[i][j] << " ";
+            for (i = 0; i < (*x); i++)
+            {
+                for (j = 0; j < (*y); j++)
+                {
+                    cout << arr[i][j] << " ";
+                }
+                cout << endl;
+            }
+            break;
         }
-        cout << endl;
+
+        default:
+            cout << "Номер операции введён неверно.";
     }
 
+    delete x;
+    delete y;
     return 0;
 }
